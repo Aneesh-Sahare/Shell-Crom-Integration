6e725b4e69f772d683ca302d700d39e9a1ebb4a8
Aneesh Sahare <145126838+Aneesh-Sahare@users.noreply.github.com>
2025-08-25 09:58:16 +0530

    Update file.sh

--- file.sh
+++ file.sh
@@ -1,5 +1,23 @@
-aws configure
+#!/bin/bash
 
-aws s3 ls
-aws ec2-describe
-aws iam list-users
+#log storage location#
+LOGFILE="home/aws/log"
+
+
+
+echo "AWS Resource Report:" >> $LOGFILE
+date >> $LOGFILE
+
+# List S3 buckets
+echo " S3 Buckets:" >> $LOGFILE
+aws s3 ls >> $LOGFILE
+
+# List EC2 instances
+echo "EC2 Instances :" >> $LOGFILE
+aws ec2 describe-instances --query "Reservations[*].Instances[*].[InstanceId,State.Name,InstanceType,PrivateIpAddress]" --output table >> $LOGFILE
+
+# List IAM users
+echo "IAM Users" >> $LOGFILE
+aws iam list-users --query "Users[*].[UserName,CreateDate]" --output table >> $LOGFILE
+
+echo "" >> $LOGFILE
